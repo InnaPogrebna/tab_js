21cbec4fb449d315a42d290e6d4dc6a47f72834d
Inna <inna_temis@ukr.net>
2022-07-02 11:42:46 +0300

    tab_js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const tabsLinks = document.querySelectorAll('.tabs-wrapper__links__link');
+const tabsContent = document
+  .querySelectorAll('.tabs-wrapper__contents__content');
+
+tabsLinks.forEach((tab, index) => {
+  tab.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    tabsLinks.forEach((link) => {
+      link.classList.remove('tabs-wrapper__links__link--active');
+    });
+
+    tab.classList.add('tabs-wrapper__links__link--active');
+
+    tabsContent.forEach((content) => {
+      content.classList.remove('tabs-wrapper__contents__content--active');
+    });
+
+    tabsContent[index].classList.add('tabs-wrapper__contents__content--active');
+  });
+});
+
+document.querySelector('.tabs-wrapper__links__link').click();
